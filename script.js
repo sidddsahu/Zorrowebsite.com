4c1b824ec2415458c00c02171270f83afb29cdb3
sidddsahu <siddhantsahu503@gmail.com>
2022-10-24 13:39:04 +0530

    sample commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+
+
+//   let card1 =document.querySelector(".page2-bottom-left h1")
+
+  
+// page2.addEventListener("mouseover",function(){
+//   h1.style.color = "black"
+//   p.style.color = "black"
+// })
